386be851dbdad0d4068b0e0854086bf5360a9479
delbury <592804814@qq.com>
2020-06-14 19:56:29 +0800

    添加资源，编写部分ui

--- gameImitator/Arknights/main.js
+++ gameImitator/Arknights/main.js
@@ -1,9 +1,35 @@
 (() => {
-  const pieces = new Pieces({
-    canvas: '#canvas',
-    text: 'Hello World',
-    items: [0]
-  });
+  // const pieces = new Pieces({
+  //   canvas: '#canvas',
+  //   text: 'Hello World',
+  //   items: [0]
+  // });
 
-  pieces.showPieces();
+  // pieces.showPieces();
+
+  bindSystemTime(); // 绑定显示时间的元素
+
+  function bindSystemTime() {
+    const ele = document.getElementById('system-time');
+    ele.innerText = getCurrentDateTime();
+
+    const fn = () => {
+      setTimeout(() => {
+        ele.innerText = getCurrentDateTime();
+        return fn();
+      }, 1000);
+    };
+    fn();
+  }
+
+  function getCurrentDateTime() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minute = date.getMinutes().toString().padStart(2, '0');
+
+    return `${year}/${month}/${day} ${hour}:${minute}`;
+  }
 })();
